2d3de01c7f0adebd1da9ebcf82c9d3819674b953
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-05-25 16:55:00 +0600

    v.3.2. schema water

--- controllers/users.js
+++ controllers/users.js
@@ -14,19 +14,21 @@ const login = async (reg, res) => {
   })
 
   const isPasswordCorrect = user && (await bcrypt.compare(password, user.password))
+  const secret = process.env.JWT_SECRET
 
-  if (user && isPasswordCorrect) {
+  if (user && isPasswordCorrect && secret) {
     return res.status(200).json({
       id: user.id,
       email: user.email,
       name: user.name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' })
     })
-  } else if (!user && isPasswordCorrect) {
-    return res.status(400).json({ message: 'Wrong login' })
-  } else if (user && !isPasswordCorrect) {
+  } else if (!user) {
+    return res.status(400).json({ message: 'Wrong email' })
+  } else if (!isPasswordCorrect) {
     return res.status(400).json({ message: 'Wrong password' })
   } else {
-    return res.status(400).json({ message: 'Wrong login and password' })
+    return res.status(400).json({ message: 'Wrong login or password' })
   }
 }
 
@@ -34,41 +36,41 @@ const login = async (reg, res) => {
 const register = async (reg, res) => {
   const { name, email, password } = reg.body
 
-  // const registeredUserByName = await prisma.user.findFirst({
-  //   where: { name },
-  // })
-  //
-  // const registeredUserByEmail = await prisma.user.findFirst({
-  //   where: { email },
-  // })
-  //
-  // if (!name || !email || !password) {
-  //   return res.status(400).json({ message: 'Input required fields, please' })
-  // } else if (registeredUserByName) {
-  //   return res.status(400).json({ message: 'User with this name already exists' })
-  // } else if (registeredUserByEmail) {
-  //   return res.status(400).json({ message: 'User with this email already exists' })
-  // }
-
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: 'Input required fields, please' })
-  }
-
   const registeredUserByName = await prisma.user.findFirst({
     where: { name },
   })
-
-  if (registeredUserByName) {
-    return res.status(400).json({ message: 'User with this name already exists' })
-  }
 
   const registeredUserByEmail = await prisma.user.findFirst({
     where: { email },
   })
 
-  if (registeredUserByEmail) {
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Input required fields, please' })
+  } else if (registeredUserByName) {
+    return res.status(400).json({ message: 'User with this name already exists' })
+  } else if (registeredUserByEmail) {
     return res.status(400).json({ message: 'User with this email already exists' })
   }
+
+  // if (!name || !email || !password) {
+  //   return res.status(400).json({ message: 'Input required fields, please' })
+  // }
+  //
+  // const registeredUserByName = await prisma.user.findFirst({
+  //   where: { name },
+  // })
+  //
+  // if (registeredUserByName) {
+  //   return res.status(400).json({ message: 'User with this name already exists' })
+  // }
+  //
+  // const registeredUserByEmail = await prisma.user.findFirst({
+  //   where: { email },
+  // })
+  //
+  // if (registeredUserByEmail) {
+  //   return res.status(400).json({ message: 'User with this email already exists' })
+  // }
 
   const salt = await bcrypt.genSalt(10)
   const hashedPassword = await bcrypt.hash(password, salt)
