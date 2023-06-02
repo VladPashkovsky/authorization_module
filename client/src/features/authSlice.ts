6d537156baea23010de439f642754e17dfdb6922
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-06-02 23:17:47 +0600

    v.8.1. authSlice

--- client/src/features/authSlice.ts
+++ client/src/features/authSlice.ts
@@ -1,22 +1,28 @@
-import {User} from '@prisma/client'
+import { User } from '@prisma/client'
 import { createSlice } from '@reduxjs/toolkit'
-import {authApi} from '../app/services/authApi'
-import {RootState} from '../app/store'
+import { authApi } from '../app/services/authApi'
+import { RootState } from '../app/store'
 
 interface AuthState {
-  user: User & {token: string} | null,
+  user: User & { token: string } | null,
   isAuth: boolean
 }
 
 const initialState: AuthState = {
   user: null,
-  isAuth: false
+  isAuth: false,
 }
 
 export const authSlice = createSlice({
-  name: '',
+  name: 'auth',
   initialState,
   reducers: {
-
-  }
+    logout: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(authApi.endpoints.login.matchFulfilled, (state, action) => {
+      state.user = action.payload
+      state.isAuth = true
+    })
+  },
 })
