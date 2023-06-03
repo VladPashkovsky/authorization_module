4646478c525e469ad7977de9e8cced367ebc38c6
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-06-03 21:47:41 +0600

    v.8.2. authSlice_store_updated

---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,43 @@
+import { User } from '@prisma/client'
+import { createSlice } from '@reduxjs/toolkit'
+import { authApi } from '../../app/services/authApi'
+import {RootState} from '../../app/store'
+
+interface AuthState {
+  user: User & { token: string } | null,
+  isAuth: boolean
+}
+
+const initialState: AuthState = {
+  user: null,
+  isAuth: false,
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(authApi.endpoints.login.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+      .addMatcher(authApi.endpoints.register.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+      .addMatcher(authApi.endpoints.current.matchFulfilled, (state, action) => {
+        state.user = action.payload
+        state.isAuth = true
+      })
+  },
+})
+
+export const { logout } = authSlice.actions
+export default authSlice.reducer
+
+export const selectIsAuth = (state: RootState) => state.auth.isAuth
+export const selectUser = (state: RootState) => state.auth.user
