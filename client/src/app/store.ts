b82582b4121c8470c288358de574ad29b043e2eb
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-06-13 23:25:40 +0600

    v.10.0. home_page

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,7 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import waterReducer from '../features/water/waterSlice'
-import { apiAuth } from './services/api'
+import { apiAuth, apiWater } from './services/api'
 import { authMiddleware } from '../middleware/authMiddleware'
 
 export const store = configureStore({
@@ -9,10 +9,11 @@ export const store = configureStore({
     authReducer,
     waterReducer,
     [apiAuth.reducerPath]: apiAuth.reducer,
+    [apiWater.reducerPath]: apiWater.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware()
-      .concat(apiAuth.middleware)
+      .concat(apiAuth.middleware, apiWater.middleware)
       .prepend(authMiddleware.middleware),
 })
 
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,10 +1,56 @@
 import React, { FC } from 'react'
 import LayoutBasic from '../../components/layoutBasic/LayoutBasic'
+import { Table } from 'antd'
+import { useGetAllWatersQuery } from '../../app/services/api'
+import type { ColumnsType } from 'antd/es/table'
+import { Water } from '@prisma/client'
+import { useNavigate } from 'react-router-dom'
+import { Paths } from '../../paths'
+
+const columnsWater: ColumnsType<Water> = [
+  {
+    title: 'Brand',
+    dataIndex: 'brand',
+    key: 'brand',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'Details',
+    dataIndex: 'details',
+    key: 'details',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'Image',
+    dataIndex: 'imageUrl',
+    key: 'details',
+  },
+]
 
 const Home: FC = () => {
+  const { data, isLoading } = useGetAllWatersQuery()
+  const navigate = useNavigate()
+
   return (
     <LayoutBasic>
-      <h1>Home PAGE</h1>
+      <Table style={{opacity: '0.85'}}
+        loading={isLoading}
+        dataSource={data}
+        pagination={false}
+        columns={columnsWater}
+        rowKey={(record) => record.id}
+        onRow={(record) => {
+          return { onClick: () => navigate(`${Paths.water}/${record.id}`) }
+        }}
+      />
     </LayoutBasic>
   )
 }
