452f27d2e61843729b477742e5b32b80a9958b72
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-07-06 23:20:56 +0600

    v.16.3. waterPage_update

--- client/src/pages/water/WaterPage.tsx
+++ client/src/pages/water/WaterPage.tsx
@@ -5,32 +5,50 @@ import { useGetWaterByIdQuery, useRemoveWaterMutation } from '../../app/services
 import { useAppSelector } from '../../app/hooks'
 // import { selectUser } from '../../features/auth/authSlice'
 import { Descriptions } from 'antd'
+import ButtonOne from '../../components/buttons/ButtonOne'
+import './waterPage.css'
 
 
 const WaterPage: FC = () => {
   const [error, setError] = useState('')
   const [isModalOpen, setIsModalOpen] = useState(false)
   const navigate = useNavigate()
-  const params = useParams<{id: string}>()
-  const {data, isLoading} = useGetWaterByIdQuery(params.id || '')
+  const params = useParams<{ id: string }>()
+  const { data, isLoading } = useGetWaterByIdQuery(params.id || '')
   const [removeWater] = useRemoveWaterMutation()
-  // const user = useAppSelector(selectUser)
-  const {user} = useAppSelector(state => state.authReducer)
-  const {waters} = useAppSelector(state => state.waterReducer)
+  const { user } = useAppSelector(state => state.authReducer)
+  const { waters } = useAppSelector(state => state.waterReducer)
 
 
   return (
     <LayoutBasic>
-      <div style={{background: 'lightgray', opacity: '0.95', width: '80%', margin: '20px'}}>
-          <Descriptions title='INFORMATION' layout='vertical' bordered>
-            <Descriptions.Item label='WaterPage ID' >{params.id}</Descriptions.Item>
-            <Descriptions.Item label='Brand' >{data && data.brand}</Descriptions.Item>
+      <div className='waterForm_basic'>
+        <div className='waterForm_container'>
+          <Descriptions style={{margin: '20px'}} title='INFORMATION' layout='vertical' bordered>
+            <Descriptions.Item label='WaterPage ID'>{params.id}</Descriptions.Item>
+            <Descriptions.Item label='Brand'>{data && data.brand}</Descriptions.Item>
             <Descriptions.Item label='Description'>{data && data.description}</Descriptions.Item>
             <Descriptions.Item label='Details'>{data && data.details}</Descriptions.Item>
             <Descriptions.Item label='Price'>{data && data.price}</Descriptions.Item>
-            <Descriptions.Item label='Image'> {<img src={data && data.imageUrl} alt='' style={{height: '30vh'}} />} </Descriptions.Item>
+            <Descriptions.Item label='Image'>
+              {<img src={data && data.imageUrl} alt='' style={{ height: '30vh' }} />}
+            </Descriptions.Item>
             <Descriptions.Item label='User'>{data && data.userId}</Descriptions.Item>
           </Descriptions>
+          <div className='button_container'>
+            <div className='all_buttons'>
+              <div>
+                <ButtonOne>Back</ButtonOne>
+              </div>
+              <div>
+                <ButtonOne>Edit</ButtonOne>
+              </div>
+              <div>
+                <ButtonOne>Delete</ButtonOne>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </LayoutBasic>
   )
